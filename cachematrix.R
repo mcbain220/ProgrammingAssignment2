d7f20d56b251d02cb7cb4db8fe2dbbe92be4d688
Chris Keaney <chris.keaney@gmail.com>
2015-04-23 08:36:09 -0400

    completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions create the getter/setter methods of caching and retrieving
+## the inversion of a matrix.
 
-## Write a short comment describing this function
+
+## This function creates the list of functions to get/set the value of the
+## original and inverted matrices.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinvert <- function(solve) m <<- solve
+    getinvert <- function() m
+    list(set = set, get = get, setinvert = setinvert, getinvert = getinvert)
 }
 
 
-## Write a short comment describing this function
+## This function checks to see if the inverted matrix is cached or has changed
+## and stores the updated inverted matrx.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinvert()
+        if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+        }
+        data <- x$get()
+        m <- solve(data,...)
+        x$setinvert(m)
+        m
 }
+
